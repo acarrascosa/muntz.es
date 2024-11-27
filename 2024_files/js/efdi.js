aa9baa3779e467ff2e7eb109dcfe5a61b5a8d266
acarrascosa <acarrascosa@optaresolutions.com>
2024-11-27 19:15:18 +0100

    added menu and missing pages and reestructure of index

--- 2024_files/js/efdi.js
+++ 2024_files/js/efdi.js
@@ -1,36 +1,18 @@
 $(document).ready(function() {
-    var audio = document.getElementById('background-audio');
     var intro = document.getElementById('intro-audio');
-    var soundIcon = document.getElementById('sound-icon');
-    var isPlaying = false;
+    var hasPlayed = true; // Variable para verificar si ya se reprodujo
 
-    soundIcon.addEventListener('click', function() {
-        if (isPlaying) {
-            audio.pause();
-			beepSound.pause();
-			beepSound.currentTime = 0;
-            soundIcon.src = '2024_files/media/sound-off.svg';
-        } else {
-            audio.play();
-            soundIcon.src = '2024_files/media/sound-on.svg';
-        }
-        isPlaying = !isPlaying;
+    intro.addEventListener('ended', function () {
+        // Cuando el audio termine de reproducirse, marcamos que ya ha sido reproducido
+        hasPlayed = true;
     });
-	
-	document.addEventListener('visibilitychange', function() {
-        if (document.hidden) {
-            // Pausa el audio si la pestaña no es visible
-            if (isPlaying) {
-                audio.pause();
-				beepSound.pause();
-				beepSound.currentTime = 0;
-                soundIcon.src = '2024_files/media/sound-off.svg';
-            }
-        } else {
-            // Reanuda el audio si la pestaña es visible y estaba en reproducción
-            if (isPlaying) {
-                audio.play();
-                soundIcon.src = '2024_files/media/sound-on.svg';
+
+    document.addEventListener('visibilitychange', function () {
+        if (!hasPlayed) { // Solo reproducir si aún no se ha reproducido completamente
+            if (document.hidden) {
+                intro.pause();
+            } else {
+                intro.play();
             }
         }
     });
@@ -44,10 +26,11 @@ $(document).ready(function() {
     var percDisplay = 0;
     var content = document.querySelector('.banner');
     var title = document.querySelector('.title');
-    var botones = document.querySelector('.botones');
-    var btnSpotify = document.querySelector('.btnSpotify');
-    var btnYoutube = document.querySelector('.btnYoutube');
-    var btnInstagram = document.querySelector('.btnInstagram');
+    // var botones = document.querySelector('.botones');
+    var countdown = document.querySelector('.countdown');
+    // var btnSpotify = document.querySelector('.btnSpotify');
+    // var btnYoutube = document.querySelector('.btnYoutube');
+    // var btnInstagram = document.querySelector('.btnInstagram');
     var btnEnter = document.querySelector('.btnEnter');
 
     var imagePromises = Array.from(images).map(function(img) {
@@ -77,25 +60,29 @@ $(document).ready(function() {
     });
 
     btnEnter.addEventListener('click', function () {
+        hasPlayed = false;
         preloader.classList.add('fade-out');
         setTimeout(function () {
             intro.play();
             content.classList.add('fade-in');
+            preloader.classList.remove('fade-in');
             preloader.style.display = 'none';
+            btnEnter.style.display = 'none';
             setTimeout(function () {
                 title.classList.add('fade-in');
                 setTimeout(function () {
                     title.classList.add('title-small');
-                    botones.classList.add('show');
-                    setTimeout(function () {
-                        btnSpotify.classList.add('show');
-                        setTimeout(function () {
-                            btnYoutube.classList.add('show');
-                            setTimeout(function () {
-                                btnInstagram.classList.add('show');
-                            }, 500);
-                        }, 500);
-                    }, 500);
+                    // botones.classList.add('show');
+                    countdown.classList.add('show');
+                    // setTimeout(function () {
+                    //     btnSpotify.classList.add('show');
+                    //     setTimeout(function () {
+                    //         btnYoutube.classList.add('show');
+                    //         setTimeout(function () {
+                    //             btnInstagram.classList.add('show');
+                    //         }, 500);
+                    //     }, 500);
+                    // }, 500);
                 }, 3000);
             }, 5000);
         }, 1000);
@@ -119,6 +106,14 @@ document.addEventListener('touchmove', function (e) {
 
 // JavaScript: Manejo del Consentimiento de Cookies
 document.addEventListener("DOMContentLoaded", function() {
+    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    const isMobile = /android|iphone|ipad|ipod|windows phone/i.test(userAgent);
+    if (isMobile) {
+        document.getElementById("linkSpotify").href = "spotify:artist:4r8sQFACB010CoU30E9yio";
+        document.getElementById("linkYoutube").href = "youtube://youtube.com/muntzband";
+        document.getElementById("linkInstagram").href = "instagram://user?username=muntzband";
+    }
+
     var cookieBanner = document.getElementById('cookie-banner');
     var acceptCookiesButton = document.getElementById('accept-cookies');
 
@@ -149,4 +144,31 @@ document.addEventListener("DOMContentLoaded", function() {
         enableGoogleAnalytics();
     }
 });
+
+// Fecha de destino
+const targetDate = new Date("2024-12-13T00:00:00").getTime();
+
+// Actualizar cada segundo
+const countdownInterval = setInterval(() => {
+    const now = new Date().getTime();
+    const timeLeft = targetDate - now;
+
+    // Calcular días, horas, minutos y segundos restantes
+    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+    // Mostrar en el DOM
+    document.getElementById("days").textContent = days.toString().padStart(2, '0');
+    document.getElementById("hours").textContent = hours.toString().padStart(2, '0');
+    document.getElementById("minutes").textContent = minutes.toString().padStart(2, '0');
+    document.getElementById("seconds").textContent = seconds.toString().padStart(2, '0');
+
+    // Detener cuando llegue a 0
+    if (timeLeft < 0) {
+        clearInterval(countdownInterval);
+        document.getElementById("countdown").textContent = "¡Evento iniciado!";
+    }
+}, 1000);
 
---
+++ 2024_files/js/menu.js
@@ -0,0 +1,44 @@
+const menuButton = document.getElementById('menu-button');
+const sideMenu = document.getElementById('side-menu');
+const preloader = document.getElementById('preloader');
+
+// Toggle para abrir/cerrar menú y cambiar el icono
+menuButton.addEventListener('click', () => {
+    sideMenu.classList.toggle('open');
+    menuButton.classList.toggle('active');
+
+    // Cambia entre íconos de "hamburguesa" y "cerrar"
+    const icon = menuButton.querySelector('i');
+    if (icon.classList.contains('fa-bars')) {
+        icon.classList.replace('fa-bars', 'fa-times');
+    } else {
+        icon.classList.replace('fa-times', 'fa-bars');
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Detectar si el dispositivo es móvil
+    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    const isMobile = /android|iphone|ipad|ipod|windows phone/i.test(userAgent);
+
+    if (isMobile) {
+        const socialLinks = document.querySelector('.social-links');
+
+        // Modificar enlaces para dispositivos móviles
+        const links = socialLinks.querySelectorAll('a');
+        links.forEach(link => {
+            const href = link.getAttribute('href');
+
+            if (href.includes('spotify.com')) {
+                link.setAttribute('href', 'spotify:artist:4r8sQFACB010CoU30E9yio');
+            } else if (href.includes('youtube.com')) {
+                link.setAttribute('href', 'youtube://youtube.com/muntzband');
+            } else if (href.includes('instagram.com')) {
+                link.setAttribute('href', 'instagram://user?username=muntzband');
+            } else if (href.includes('x.com')) {
+                link.setAttribute('href', 'twitter://user?screen_name=muntzband');
+            }
+            // Bandcamp no necesita cambios
+        });
+    }
+});
